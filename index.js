a276765ec486c5813f1cbbffed5448006e428cca
CarlaDCyr <118022663+CarlaDCyr@users.noreply.github.com>
2022-12-07 22:26:38 -0700

    Updated HTML & JS files
    Updated html to compliment javascript functionality. Added javascript addition functions

---
+++ index.js
@@ -0,0 +1,34 @@
+let home = document.getElementById("home-score")
+let guest = document.getElementById("guest-score")
+let homeScore = home.textContent
+let guestScore = guest.textContent
+
+function addOneHome(){
+    homeScore = +homeScore + 1
+    home.textContent = homeScore
+}
+
+function addTwoHome(){
+    homeScore = +homeScore + 2
+    home.textContent = homeScore
+}
+
+function addThreeHome(){
+    homeScore = +homeScore + 3
+    home.textContent = homeScore
+}
+
+function addOneGuest(){
+    guestScore = +guestScore + 1
+    guest.textContent = guestScore
+}
+
+function addTwoGuest(){
+    guestScore = +guestScore + 2
+    guest.textContent = guestScore
+}
+
+function addThreeGuest(){
+    guestScore = +guestScore + 3
+    guest.textContent = guestScore
+}
